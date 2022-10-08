7ca994fcbc8982d1a17d3c1b09136d0cdefaccd3
Vikas <vikaschoudharyfr@gmail.com>
2022-10-08 21:59:58 +0530

    Add tempConversion logic

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let newTemp = (temp - 32)* 5/9;
+  newTemp = +newTemp.toFixed(1);
+  return newTemp;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let newTemp = (temp * 9/5) + 32;
+  newTemp = +newTemp.toFixed(1);
+  return newTemp;
 };
 
 // Do not edit below this line
