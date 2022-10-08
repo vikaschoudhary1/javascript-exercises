d03fda5410b0d8af7c29e6c87d67e0602c8f6261
Vikas <vikaschoudharyfr@gmail.com>
2022-10-08 20:25:38 +0530

    Complete js exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = (arr, ...arg) => {
+    for (let i=0; i < arg.length; i++){
+        const varPosition = arr.indexOf(arg[i]);
+        if ( varPosition === -1){ continue;}        //resets the loop if cannot find arg[i] in arr
+        arr.splice(varPosition, 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
