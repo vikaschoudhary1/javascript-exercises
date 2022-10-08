426c5fb6ec06df71151ba1d87f0e9b99dd0a0e2a
Vikas <vikaschoudharyfr@gmail.com>
2022-10-08 10:37:36 +0530

    Finish e02 of javascript exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = (str, n) => {
+   if(n >= 0){ 
+        let arrStr = [];
+        for (let i = 0; i < n; i++){
+            arrStr.push(str);
+        }
+        return arrStr.join('');
+   } else { return 'ERROR'}
 };
 
 // Do not edit below this line
